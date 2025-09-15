f622b5842bc15dc840747c5697f8e886c571614f
Shivam Upadhyay <38035252+itsshivamupadhyay@users.noreply.github.com>
2025-09-16 04:05:59 +0700

    Configure S3 backend for Terraform state management

--- task1/backend.tf
+++ task1/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "shivam-terraform-state"
+    key            = "shivam-devops/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
